b5219486f67376182017b543dc0ac50e6d85f031
Sasha Bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2023-06-28 20:59:03 +0100

    Get rid of deprecated set-output

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -9,7 +9,7 @@ echo "::add-mask ::$1"
 echo "Hello $1"
 
 time=$(date)
-echo "::set-output name=time::$time"
+echo "{time}={$time}" >> $GITHUB_OUTPUT
 
 echo "::group::Some expandable logs"
 echo "Lorem Ipsum,\nwhat was next?"
