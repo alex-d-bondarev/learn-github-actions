c88dafdb7dc766ffb261abcfbefe09a07a172375
Sasha Bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2023-06-28 20:44:51 +0100

    Add docker action

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -l
+
+echo "::debug ::This is a debug message"
+echo "::warning ::This is a warning message"
+echo "::error ::This is an error message"
+
+echo "::add-mask ::$1"
+
+echo "Hello $1"
+
+time=$(date)
+echo "::set-output name=time::$time"
+
+echo "::group::Some expandable logs"
+echo "Lorem Ipsum,\nwhat was next?"
+echo "Lorem ipsum dolor sit amet,"
+echo "Yes!"
+echo "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+echo "::endgroup::"
+
+echo "::set-env name=HELLO::hello env var"
+printenv | grep HELLO
