313a7136e08ad273f06a63fd5958259e059f7866
Sasha Bondarev <5994478+alex-d-bondarev@users.noreply.github.com>
2023-06-28 21:05:38 +0100

    Print name before masking it too

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -4,9 +4,9 @@ echo "::debug ::This is a debug message"
 echo "::warning ::This is a warning message"
 echo "::error ::This is an error message"
 
-echo "::add-mask ::$1"
-
 echo "Hello $1"
+echo "::add-mask ::$1"
+echo "Hello $1 (name is expected to be masked now)"
 
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
